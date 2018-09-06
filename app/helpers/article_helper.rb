962f55a24a1217ce2a5d1097b11db51e32611f74
foolingdutchman <365300312@qq.com>
2018-09-06 10:13:45 +0800

    small issue

--- app/helpers/article_helper.rb
+++ app/helpers/article_helper.rb
@@ -1,2 +1,13 @@
 module ArticleHelper
+
+    def getIcon(article)
+        if article.category =="share"
+             "/img/t_info.png"
+        elsif article.category=="mining"
+             "/img/t_mining.png"
+        else
+             "/img/t_app.png"
+        end
+        
+    end
 end
